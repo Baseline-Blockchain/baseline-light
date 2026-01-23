0f4eba394ceb7efd8eb731e59a68498c5d41cebf
crosschainer <eodkwokm@gmail.com>
2026-01-23 21:21:24 +0100

    android

---
+++ src-tauri/gen/android/app/src/main/java/com/baseline/light/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.baseline.light
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
